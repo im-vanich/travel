6ffea68c457e876e179a8703d2ffe50c68d5c1f2
Ivan Matveev <skiforceone@gmail.com>
2020-07-09 18:15:18 +0300

    Correct information about tour in tour.html

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
-from data import *
+from data import tours, departures
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index_render():
     return render_template("index.html")
 
 
-@app.route("/departures/<departure>/")
-def departures(departure):
-    return render_template("departure.html")
+@app.route('/departures/<departure>/')
+def departures_render(departure):
+    return render_template('departure.html')
 
 
-@app.route("/tours/<int:id>/")
-def tours(id):
-    return render_template("tour.html")
+@app.route('/tours/<int:id>/')
+def tours_render(id):
+    return render_template('tour.html', tours_data=tours[id], departures=departures)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
