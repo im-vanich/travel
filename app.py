9ac1af0259b3897f3f011df07224009eae40e1b3
Ivan Matveev <skiforceone@gmail.com>
2020-07-12 23:21:57 +0300

    Изменил подключение style.css в базавом шаблоне. Добавил папку css в static

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 from data import tours, departures, title, subtitle, description
 
 app = Flask(__name__)
